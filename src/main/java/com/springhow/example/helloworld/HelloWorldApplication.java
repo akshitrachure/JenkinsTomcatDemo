59043a51a2f32d0a4ef90a427284cd0cba13990d
akshitrachure <52562512+akshitrachure@users.noreply.github.com>
2021-11-12 16:22:49 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "First Deployment";
+        return "Hello World";
     }
 
 }
